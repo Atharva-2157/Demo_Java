94c552f5c5d8fe39961a83790cc569b63a276360
Atharva-2157 <atharvadiwan65@gmail.com>
2020-07-05 11:19:48 +0530

    Initial commit

--- src/game/Main.java
+++ src/game/Main.java
@@ -10,83 +10,101 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.ActionListener;
 
-class Gameplay extends JPanel implements KeyListener, ActionListener
-{
+public class Main extends JPanel implements KeyListener, ActionListener{
+
     private int pos_x;
-    private int pos_y;
+    private final int pos_y;
     private int change_in_x;
-    private int change_in_y;
+    private static final int screen_width;
+    private static final int screen_height;
+
+    static
+    {
+        screen_width = 500;
+        screen_height = 500;
+    }
+
+    class Player
+    {
+        private int player_w;
+        private int player_h;
+        private int player_x;
+        private int player_y;
 
 
-    public Gameplay()
+    }
+
+    public Main()
     {
         this.pos_x = 10;
-        this.pos_y = 20;
+        this.pos_y = 10;
         this.change_in_x = 0;
-        this.change_in_y = 0;
-        Timer timer = new Timer(110, this);
-        timer.start();
+        Timer t = new Timer(5, this);
+        t.start();
     }
-
-    @Override
-    public void keyTyped(KeyEvent keyEvent) {
-    }
-
-    @Override
-    public void keyPressed(KeyEvent keyEvent) {
-        int change = 10;
-        if(keyEvent.getKeyCode() == KeyEvent.VK_RIGHT) {
-            this.change_in_x = change;
-            this.change_in_y = 0;
-        }
-        else if(keyEvent.getKeyCode() == KeyEvent.VK_LEFT) {
-            this.change_in_x = -change;
-            this.change_in_y = 0;
-        }
-        else if(keyEvent.getKeyCode() == KeyEvent.VK_UP){
-            this.change_in_x = 0;
-            this.change_in_y = -change;
-        }
-        else if(keyEvent.getKeyCode() == KeyEvent.VK_DOWN) {
-            this.change_in_x = 0;
-            this.change_in_y = change;
-        }
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent keyEvent) {
-
-    }
-
-    @Override
-    public void paint(Graphics graphics) {
-        graphics.setColor(Color.GREEN);
-        graphics.drawRect(pos_x, pos_y, 20, 20);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent actionEvent) {
-        this.pos_x += this.change_in_x;
-        this.pos_y += this.change_in_y;
-        repaint();
-    }
-}
-
-public class Main {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        Gameplay game = new Gameplay();
+        JFrame frame = new JFrame("Spaced Invaders");
+        Main game = new Main();
 
         frame.add(game);
         frame.addKeyListener(game);
 
-        frame.setBounds(300, 50, 500, 500);
+        frame.setBounds(300, 50, screen_width, screen_height);
         frame.setVisible(true);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.setColor(Color.GREEN);
+        g.fillRect(this.pos_x, this.pos_y, 20, 30);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(this.pos_x < 0)
+        {
+            this.change_in_x = 0;
+            this.pos_x = 0;
+        }
+
+        if(this.pos_x > screen_width - 30)
+        {
+            this.change_in_x = 0;
+            this.pos_x = screen_width - 30;
+        }
+
+        this.pos_x += this.change_in_x;
+
+        repaint();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int c = e.getKeyCode();
+
+        final int change = 1;
+        if(c == KeyEvent.VK_LEFT)
+        {
+            this.change_in_x = -change;
+        }
+        if(c == KeyEvent.VK_RIGHT)
+        {
+            this.change_in_x = change;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        this.change_in_x = 0;
+    }
 }
